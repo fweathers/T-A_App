79b4f2e252f102f962444f5a5dafa40b59ad12de
Felicia Weathers <Fel.Weathers@gmail.com>
2017-01-04 20:44:12 -0500

    first commit

---
+++ T&A_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T&A_App
+//
+//  Created by Felicia Weathers on 1/4/17.
+//  Copyright © 2017 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
